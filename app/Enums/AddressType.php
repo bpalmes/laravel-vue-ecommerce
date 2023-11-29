86ddbf98aa41db8488409b6708b4e22e93d142c2
bpalmes <rmnnpalmes@gmail.com>
2023-11-29 17:19:59 +0000

    segundo

---
+++ app/Enums/AddressType.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Enums;
+
+
+/**
+ * Class AddressType
+ *
+ *  
+ * @package App\Enums
+ */
+enum AddressType: string
+{
+    case Shipping = 'shipping';
+    case Billing = 'billing';
+}
---
+++ app/Enums/PaymentStatus.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Enums;
+
+
+/**
+ * Class PaymentStatus
+ *
+ *  
+ * @package App\Enums
+ */
+enum PaymentStatus: string
+{
+    case Pending = 'pending';
+    case Paid = 'paid';
+    case Failed = 'failed';
+}
